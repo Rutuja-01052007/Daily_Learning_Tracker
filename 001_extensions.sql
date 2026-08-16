28228974df950e7e30ce04391f8a8973c180535b
Dhruv Talmale <dhruvtalmale435@gmail.com>
2026-08-16 14:40:01 +0530

    Delete 001_extensions.sql

--- 001_extensions.sql
+++
@@ -1,5 +0,0 @@
--- Migration: 001_extensions
--- Description: Enables required PostgreSQL extensions (PostGIS for geographical data and pgcrypto for UUID/hash operations).
-
-CREATE EXTENSION IF NOT EXISTS postgis;
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
